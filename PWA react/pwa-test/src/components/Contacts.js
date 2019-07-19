490218c86df1b7e58d930d6f7a3bc7bb55a4d902
user <sami.elnakadi@gmail.com>
2019-07-19 15:13:27 +0200

    3

--- PWA react/pwa-test/src/components/Contacts.js
+++ PWA react/pwa-test/src/components/Contacts.js
@@ -5,35 +5,40 @@ export default class Contacts extends Component {
     render() {
       return (
         <section className="Contacts container">
-          <h3 className="titre">Nos contacter</h3>
-          <div className="row">
-            <form>
-              <div className="col-md-6">
+          <h3 className="titre3">Nous contacter</h3>
+          
+            <form className="row">
+              <div className="col-md-4">
+
                 <div className="form-group">
                 <label for="inputname">Nom:</label>
                 <input type="text" name="lname"></input>
                 </div>
+
                 <div className="form-group">
                 <label for="inputFname">Prénom:</label>
                 <input type="text" name="fname"></input>
                 </div>
-              </div>
-              <div className="col-md-6">
+              
+              
                 <div className="form-group">
                 <label for="inputmessSub">Sujet du message:</label>
                 <input type="text" name="fname"></input>
                 </div>
+
                 <div className="form-group">
                   <label for="inputemail">Adresse Email:</label>
                   <input type="email" id="mail" placeholder="Entrez votre e-mail"></input>
                 </div>
               </div>
+              <div className="col-md-8">
                 <div className="form-group">
-                <label for="inputmess">Message</label>
-                <textarea type="text" name="message"  rows="4" cols="50"></textarea>
+                <label for="inputmess">Message:</label>
+                <textarea type="text" name="message"  rows="6" cols="100"></textarea>
                 </div>
+              </div>  
             </form>
-          </div>
+          
         </section>
       );
     }
